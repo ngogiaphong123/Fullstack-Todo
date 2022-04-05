8fe5ed71fb0747370f9781a8dda86870fd7bd605
Ngo Gia Phong <76879620+ngogiaphong123@users.noreply.github.com>
2022-04-05 13:19:51 +0700

    Deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
+const dotenv = require("dotenv");
+
+dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
